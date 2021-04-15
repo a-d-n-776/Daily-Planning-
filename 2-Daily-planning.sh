5cfb48965744f498e9b8cc595143b042df101e9d
NAVEEN <76776509+a-d-n-776@users.noreply.github.com>
2021-04-15 11:25:43 +0530

    Create 2-Daily-planning.sh

---
+++ 2-Daily-planning.sh
@@ -0,0 +1,19 @@
+2.Daily-planning
+
+Microsoft Windows [Version 10.0.18363.1316]
+(c) 2019 Microsoft Corporation. All rights reserved.
+
+C:\Users\MN0116\Desktop\Naveen\AWS\Management>scp -i mana.pem ec2-ubuntu@ec2-176-34-135-53.eu-west-1.compute.amazonaws.com:/devops/sh/a0.sh /Users/MN0116/Downloads
+The authenticity of host 'ec2-176-34-135-53.eu-west-1.compute.amazonaws.com (176.34.135.53)' can't be established.
+ECDSA key fingerprint is SHA256:S/kWYmtz71sC02sfzdDRsybCAU0q/0YofEWvVN/WMWk.
+Are you sure you want to continue connecting (yes/no)?
+Warning: Permanently added 'ec2-176-34-135-53.eu-west-1.compute.amazonaws.com,176.34.135.53' (ECDSA) to the list of known hosts.
+Load key "mana.pem": invalid format
+ec2-ubuntu@ec2-176-34-135-53.eu-west-1.compute.amazonaws.com's password:
+Permission denied, please try again.
+ec2-ubuntu@ec2-176-34-135-53.eu-west-1.compute.amazonaws.com's password:
+Permission denied, please try again.
+ec2-ubuntu@ec2-176-34-135-53.eu-west-1.compute.amazonaws.com's password:
+ec2-ubuntu@ec2-176-34-135-53.eu-west-1.compute.amazonaws.com: Permission denied (publickey,password).
+
+C:\Users\MN0116\Desktop\Naveen\AWS\Management>
